397f5eabf36e4359810ef06fcc035396d78cba44
T Benson <t.benson@live.co.uk>
2019-12-02 19:27:50 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -12,26 +12,23 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
-
-  # Consult README for inputs and outputs
-  #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-  consolidated_cart = cart
-
-  new_cart_index = 0
-  item_count = 0
-    while new_cart_index < consolidated_cart.count do
-      item_name = consolidated_cart[new_cart_index][:item]
-      old_cart_index = 0
-        while old_cart_index < cart.length do
-          item_count +=1 if cart[old_cart_index][:item] == item_name
-          old_cart_index += 1
-        end
-      consolidated_cart[:count] = item_count
-      new_cart_index +=1
-    end
-  consolidated_cart.uniq!
+  unique_cart = cart.uniq
+  # 
+  # new_cart_index = 0
+  # item_count = 0
+  #   while new_cart_index < consolidated_cart.count do
+  #     item_name = consolidated_cart[new_cart_index][:item]
+  #     old_cart_index = 0
+  #       while old_cart_index < cart.length do
+  #         item_count +=1 if cart[old_cart_index][:item] == item_name
+  #         old_cart_index += 1
+  #       end
+  #     consolidated_cart[:count] = item_count
+  #     new_cart_index +=1
+  #   end
+  unique_cart
   # binding.pry
 end
 # second attempt
