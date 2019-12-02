89674b35ec8533a858f86d101a31ef6458e13d83
T Benson <t.benson@live.co.uk>
2019-12-02 19:27:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -15,7 +15,7 @@ def consolidate_cart(cart)
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
   unique_cart = cart.uniq
-  # 
+  #
   # new_cart_index = 0
   # item_count = 0
   #   while new_cart_index < consolidated_cart.count do
